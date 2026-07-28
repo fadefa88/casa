6ead0710991fd822df330672dd62e65e661a2e21
fadefa88 <62143875+fadefa88@users.noreply.github.com>
2026-07-28 23:24:27 +0200

    Aggiunge sensori rete FRITZBox e backup 5G

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 window.CASA_DASHBOARD_CONFIG = {
   mode: 'demo',
   refreshMs: 5000,
-  homeAssistant: {
-    url: '',
-    token: ''
-  },
+  homeAssistant: { url: '', token: '' },
   entities: {
     housePower: 'sensor.house_power',
     houseToday: 'sensor.house_today_energy',
@@ -22,33 +19,36 @@ window.CASA_DASHBOARD_CONFIG = {
     heatPumpToday: 'sensor.heat_pump_today_energy',
     heatPumpMonth: 'sensor.heat_pump_month_energy',
     heatPumpMode: 'sensor.heat_pump_mode',
-
     inductionPower: 'sensor.induction_power',
     inductionSession: 'sensor.induction_session_energy',
     inductionToday: 'sensor.induction_today_energy',
     inductionPeak: 'sensor.induction_peak_power',
 
-    washerPower: 'sensor.washer_power',
-    washerToday: 'sensor.washer_today_energy',
-    washerState: 'sensor.washer_state',
-    dryerPower: 'sensor.dryer_power',
-    dryerToday: 'sensor.dryer_today_energy',
-    dryerState: 'sensor.dryer_state',
-    ovenPower: 'sensor.oven_power',
-    ovenToday: 'sensor.oven_today_energy',
-    ovenState: 'sensor.oven_state',
-    fridgePower: 'sensor.fridge_power',
-    fridgeToday: 'sensor.fridge_today_energy',
-    fridgeState: 'sensor.fridge_state',
+    washerPower: 'sensor.washer_power', washerToday: 'sensor.washer_today_energy', washerState: 'sensor.washer_state',
+    dryerPower: 'sensor.dryer_power', dryerToday: 'sensor.dryer_today_energy', dryerState: 'sensor.dryer_state',
+    ovenPower: 'sensor.oven_power', ovenToday: 'sensor.oven_today_energy', ovenState: 'sensor.oven_state',
+    fridgePower: 'sensor.fridge_power', fridgeToday: 'sensor.fridge_today_energy', fridgeState: 'sensor.fridge_state',
 
     tvPower: 'sensor.tv_power',
     shieldPower: 'sensor.nvidia_shield_power',
     mediaPcPower: 'sensor.media_mini_pc_power',
     hddPower: 'sensor.media_hdd_power',
-
     pcPower: 'sensor.office_pc_power',
     monitorPower: 'sensor.office_monitor_power',
     ps5Power: 'sensor.ps5_power',
-    dockPower: 'sensor.office_dock_power'
+    dockPower: 'sensor.office_dock_power',
+
+    networkState: 'sensor.fritzbox_wan_status',
+    networkLinkDown: 'sensor.fritzbox_link_download_mbps',
+    networkLinkUp: 'sensor.fritzbox_link_upload_mbps',
+    networkCurrentDown: 'sensor.internet_download_mbps',
+    networkCurrentUp: 'sensor.internet_upload_mbps',
+    networkPing: 'sensor.internet_ping_ms',
+    networkJitter: 'sensor.internet_jitter_ms',
+    networkPacketLoss: 'sensor.internet_packet_loss_percent',
+    networkUptimeHours: 'sensor.fritzbox_uptime_hours',
+    networkClients: 'sensor.fritzbox_connected_devices',
+    networkWifiClients: 'sensor.fritzbox_wifi_devices',
+    backup5gStatus: 'sensor.backup_5g_status'
   }
 };
