886398271494d1bfa5146d8a18251b187612209d
fadefa88 <62143875+fadefa88@users.noreply.github.com>
2026-07-28 23:11:55 +0200

    Aggiunge entity Home Assistant per tutti i carichi monitorati

--- config.js
+++ config.js
@@ -1,27 +1,54 @@
-window.CASA_DASHBOARD_CONFIG={
-  mode:'demo',
-  refreshMs:5000,
-  homeAssistant:{url:'',token:''},
-  entities:{
-    housePower:'sensor.house_power',
-    houseToday:'sensor.house_today_energy',
-    houseCost:'sensor.house_today_cost',
-    pvPower:'sensor.fronius_power',
-    pvToday:'sensor.fronius_today',
-    gridImport:'sensor.grid_import_today',
-    gridExport:'sensor.grid_export_today',
-    tempIn:'sensor.house_temperature',
-    tempOut:'sensor.outdoor_temperature',
-    heatPumpPower:'sensor.heat_pump_power',
-    vmcPower:'sensor.vmc_power',
-    tvPower:'sensor.tv_zone_power',
-    studioPower:'sensor.studio_power',
-    inductionPower:'sensor.induction_power',
-    labPower:'sensor.lab_power',
-    routerLatency:'sensor.router_latency',
-    switchLoad:'sensor.switch_load',
-    alarmState:'alarm_control_panel.home',
-    lightsGround:'light.ground_floor',
-    lightsFirst:'light.first_floor'
+window.CASA_DASHBOARD_CONFIG = {
+  mode: 'demo',
+  refreshMs: 5000,
+  homeAssistant: {
+    url: '',
+    token: ''
+  },
+  entities: {
+    housePower: 'sensor.house_power',
+    houseToday: 'sensor.house_today_energy',
+    houseCost: 'sensor.house_today_cost',
+    housePeak: 'sensor.house_daily_peak',
+    houseVsYesterday: 'sensor.house_vs_yesterday_percent',
+
+    pvPower: 'sensor.fronius_power',
+    pvToday: 'sensor.fronius_today',
+    pvSelfConsumption: 'sensor.pv_self_consumption_percent',
+    gridImport: 'sensor.grid_import_today',
+    gridExport: 'sensor.grid_export_today',
+
+    heatPumpPower: 'sensor.heat_pump_power',
+    heatPumpToday: 'sensor.heat_pump_today_energy',
+    heatPumpMonth: 'sensor.heat_pump_month_energy',
+    heatPumpMode: 'sensor.heat_pump_mode',
+
+    inductionPower: 'sensor.induction_power',
+    inductionSession: 'sensor.induction_session_energy',
+    inductionToday: 'sensor.induction_today_energy',
+    inductionPeak: 'sensor.induction_peak_power',
+
+    washerPower: 'sensor.washer_power',
+    washerToday: 'sensor.washer_today_energy',
+    washerState: 'sensor.washer_state',
+    dryerPower: 'sensor.dryer_power',
+    dryerToday: 'sensor.dryer_today_energy',
+    dryerState: 'sensor.dryer_state',
+    ovenPower: 'sensor.oven_power',
+    ovenToday: 'sensor.oven_today_energy',
+    ovenState: 'sensor.oven_state',
+    fridgePower: 'sensor.fridge_power',
+    fridgeToday: 'sensor.fridge_today_energy',
+    fridgeState: 'sensor.fridge_state',
+
+    tvPower: 'sensor.tv_power',
+    shieldPower: 'sensor.nvidia_shield_power',
+    mediaPcPower: 'sensor.media_mini_pc_power',
+    hddPower: 'sensor.media_hdd_power',
+
+    pcPower: 'sensor.office_pc_power',
+    monitorPower: 'sensor.office_monitor_power',
+    ps5Power: 'sensor.ps5_power',
+    dockPower: 'sensor.office_dock_power'
   }
 };
